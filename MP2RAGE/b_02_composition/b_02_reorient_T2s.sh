afa55eb96512b271838284e494526c2d016be32a
ofgulban <farukgulban@gmail.com>
2017-11-06 12:24:20 +0100

    The residual BV references deleted from comments.

--- MP2RAGE/b_02_composition/b_02_reorient_T2s.sh
+++ MP2RAGE/b_02_composition/b_02_reorient_T2s.sh
@@ -3,13 +3,13 @@
 # this bash scripts helps to match headers between two files
 # before you run this make sure that both the file_header_proper and the file_header_before have each been opened in itksnap and saved.
 
-# give path to file with proper nii header (i.e. before transformations in BV)
+# give path to file with proper nii header
 file_header_proper="/home/faruk/gdrive/temp_segmentator_paper_data/MP2RAGE/S001/derived/02_composition/S001_uni_bet.nii.gz"
 
-# give path to file with broken nii header (i.e. before transformations in BV) before correction
+# give path to file with broken nii header before correction
 file_header_before="/home/faruk/gdrive/temp_segmentator_paper_data/MP2RAGE/S001/derived/02_composition/t2s/umpire_reslice.nii.gz"
 
-# give path to file with broken nii header (i.e. before transformations in BV) after correction
+# give path to file with broken nii header after correction
 file_header_after="/home/faruk/gdrive/temp_segmentator_paper_data/MP2RAGE/S001/derived/02_composition/t2s/umpire_reslice_reorient.nii.gz"
 
 # fslorient -forceradiological ${file_header_before}

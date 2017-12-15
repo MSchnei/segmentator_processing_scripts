79940cb3930fe1f517d7c38840ddb1d5f61e3f8b
ofgulban <farukgulban@gmail.com>
2017-12-15 19:05:31 +0100

    More outputs

--- Supplementary/boost_estimation.py
+++ Supplementary/boost_estimation.py
@@ -7,17 +7,20 @@ automatic segmentation mistakes.
 import numpy as np
 from nibabel import load
 
+
 # ground truth
-gt = load("/path/to/S001_gm_06_artif\act_masked.nii.gz")
+gt = load("/path/to/S019_gm_07_artifact_masked.nii.gz")
 # automatic segmentation process output (eg. fsl-fast, cbs-tools, spm)
-nii_1 = load("/path/to/S001_uni_cbs_gm_artifact_masked.nii.gz")
+nii_1 = load("/path/to/S019_uni_cbs_gm_artifact_masked.nii.gz")
 # gray matter labels after using segmentator
-nii_2 = load("/path/to/S001_uni_cbs_gm_gramag_artifact_masked.nii.gz")
+nii_2 = load("/path/to/S019_uni_cbs_gm_gramag_artifact_masked.nii.gz")
 
 # -----------------------------------------------------------------------------
 gt = gt.get_data()
 nii_1 = nii_1.get_data()
 nii_2 = nii_2.get_data()
+
+print("Ground truth GM: %i voxels" % (np.sum(gt)))
 
 gt_inv = -gt + 1
 nii_1_inv = -nii_1 + 1
@@ -31,6 +34,11 @@ nr_FP_before = np.sum(gt_inv*nii_1)
 nr_FP_after = np.sum(gt_inv*nii_2)
 nr_FP_difference = nr_FP_before - nr_FP_after
 
+nr_FN_before = np.sum(gt * nii_1_inv)
+nr_FN_after = np.sum(gt * nii_2_inv)
+
+print("FP + FN before segmentator: %i voxels" % (nr_FP_before + nr_FN_before))
+
 nr_hits_before / (nr_hits_before + nr_FP_before)
 nr_of_voxels_benefit = nr_FP_difference - nr_hits_difference
 
@@ -40,4 +48,5 @@ voxels_per_second = 1
 
 # convert to human readable time
 time_benefit = (nr_of_voxels_benefit * voxels_per_second) / 3600
+print("Boost estimation: %i voxels" % nr_of_voxels_benefit)
 print("Boost estimation: %i hours" % time_benefit)

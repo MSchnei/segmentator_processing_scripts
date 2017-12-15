d04ce1f6b7539b2d6532e398226d0d947c50728d
ofgulban <farukgulban@gmail.com>
2017-12-15 12:02:11 +0100

    using the same naming convention as in the paper

---
+++ Supplementary/boost_estimation.py
@@ -0,0 +1,43 @@
+"""Compute approximate time savings using Segmentator.
+
+The boost is estimated in comparison to the manual slice-by-slice correction of
+automatic segmentation mistakes.
+"""
+
+import numpy as np
+from nibabel import load
+
+# ground truth
+gt = load("/path/to/S001_gm_06_artif\act_masked.nii.gz")
+# automatic segmentation process output (eg. fsl-fast, cbs-tools, spm)
+nii_1 = load("/path/to/S001_uni_cbs_gm_artifact_masked.nii.gz")
+# gray matter labels after using segmentator
+nii_2 = load("/path/to/S001_uni_cbs_gm_gramag_artifact_masked.nii.gz")
+
+# -----------------------------------------------------------------------------
+gt = gt.get_data()
+nii_1 = nii_1.get_data()
+nii_2 = nii_2.get_data()
+
+gt_inv = -gt + 1
+nii_1_inv = -nii_1 + 1
+nii_2_inv = -nii_2 + 1
+
+nr_hits_before = np.sum(gt*nii_1)
+nr_hits_after = np.sum(gt*nii_2)
+nr_hits_difference = nr_hits_before - nr_hits_after
+
+nr_FP_before = np.sum(gt_inv*nii_1)
+nr_FP_after = np.sum(gt_inv*nii_2)
+nr_FP_difference = nr_FP_before - nr_FP_after
+
+nr_hits_before / (nr_hits_before + nr_FP_before)
+nr_of_voxels_benefit = nr_FP_difference - nr_hits_difference
+
+# assumed nr of voxels per second on average that can be corrected manually by
+# a human operator (eg. a poor phd student)
+voxels_per_second = 1
+
+# convert to human readable time
+time_benefit = (nr_of_voxels_benefit * voxels_per_second) / 3600
+print("Boost estimation: %i hours" % time_benefit)
